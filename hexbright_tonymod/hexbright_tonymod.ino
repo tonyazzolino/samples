f073a713b65b60b858faecfa4409e6c53c8976ae
Tony Azzolino <tony@tonyazzolino.com>
2012-12-29 21:47:56 -0800

    commented code

--- hexbright_tonymod/hexbright_tonymod.ino
+++ hexbright_tonymod/hexbright_tonymod.ino
@@ -11,6 +11,7 @@
  v1.3  Dec 24, 2012: Merged LMH and HML and added program mode to switch between them
  v1.4  Dec 24, 2012: Pulled improved dazzle from https://github.com/digitalmisery/HexBrightFLEX
  v1.5  Dec 26, 2012: Enabled HexBright to skip modes and to save modes skipped to EEPROM.
+ v1.6  Dec 29, 2012: Tweaked fast blink and pre-dazzle driver level to avoid brightness change on button release.
  
  Instructions:
  From off:
@@ -48,14 +49,15 @@
 // Settings
 #define OVERTEMP                340
 // Pin assignments
-#define DPIN_RLED_SW            2
-#define DPIN_GLED               5
-#define DPIN_PWR                8
-#define DPIN_DRV_MODE           9
-#define DPIN_DRV_EN             10
-#define APIN_TEMP               0
-#define APIN_CHARGE             3
+#define DPIN_RLED_SW            2 // Switch and red LED. Can sense button presses when the pin is on input mode, and can light the red LED when the pin is in output mode.
+#define DPIN_GLED               5 // Green LED
+#define DPIN_PWR                8 // Set to output and bring high to make the atmega168 stay on.
+#define DPIN_DRV_MODE           9 // Main LED driver power level. Valid values are digital high or low.
+#define DPIN_DRV_EN             10 // Main LED. Can use digitalWrite for on or off, or analogWrite 0-255 for native PWM.
+#define APIN_TEMP               0 // Temperature pin
+#define APIN_CHARGE             3 // Charge controller status pin. Registers either off, charging, or charged.
 // Modes
+// MODE_OFFHOLD and PRE_ modes exist to catch long presses
 #define MODE_OFF                0
 #define MODE_OFFHOLD            1
 #define MODE_PRE_LOW            2
@@ -74,21 +76,20 @@
 #define MODE_PROGRAM            15
 
 // State
-byte mode = 0;
-byte hmlbyte = 0;
-byte skiptomode = 255;
-boolean hml = false;
-boolean lowskip = false, medskip = true, highskip = false, dazskip = false, fblskip = true, sblskip = false;
-boolean mprog = false;
-unsigned long btnTime = 0;
-boolean btnDown = false;
-boolean fadeDown = false;
-int fadeCount = 0;
-int fadeWait = 0;
-int fadeOffWait = 0;
-boolean gledOff = false;
-boolean dazzle_on = true;
-long dazzle_period = 100;
+byte mode = 0; // current mode
+byte skiptomode = 255; // used for mode skipping; if set anywhere in the loop, the light will go to that mode in the next loop. 255 is used as a null value in this case.
+boolean hml = false; // true for high-medium-low operation, false for low-medium-high operation
+boolean lowskip = false, medskip = true, highskip = false, dazskip = false, fblskip = true, sblskip = false; // These are true if those modes are skipped, false if they are not
+boolean mprog = false; // HexBright enters mode skip programming if true, is normally set true by a 0.5 second press from HML/LMH select mode, and then set false by the end of programming.
+unsigned long btnTime = 0; // Button hold time
+boolean btnDown = false; // Button status
+boolean fadeDown = false; // Direction for charging LED fade
+int fadeCount = 0; // LED level for charging and HML/LMH select fade
+int fadeWait = 0; // Loop counter to set fade speed for green LED
+int fadeOffWait = 0; // Loop counter for off time between fades for green LED
+boolean gledOff = false; // Set between green LED fades to have it stay off for a time
+boolean dazzle_on = true; // Tracking variable for dazzle
+long dazzle_period = 100; // another tracking variable for dazzle
 
 void setup()
 {
@@ -113,8 +114,8 @@ void setup()
   btnDown = digitalRead(DPIN_RLED_SW);
   mode = MODE_OFF;
 
-  // Read HML mode from EEPROM
-  if (EEPROM.read(0) == 1) hml = true; 
+  // Read settings from EEPROM
+  if (EEPROM.read(0) == 1) hml = true; // Only set variables true on 1 value. All other values set the variables false.
   else hml = false;
   if (EEPROM.read(1) == 1) highskip = true; 
   else highskip = false;
@@ -163,49 +164,51 @@ void loop()
   if (mode != MODE_PROGRAM && mode != MODE_PRE_PROGRAM && !mprog){
     if (chargeState < 128)  // Low - charging
     {
-      // digitalWrite(DPIN_GLED, (time&0x0100)?LOW:HIGH);
-      if (!gledOff)
+      // digitalWrite(DPIN_GLED, (time&0x0100)?LOW:HIGH); // Old charging status code
+
+      // Green LED fade code
+      if (!gledOff) // go to wait mode if gledOff is set
       {
-        if (!fadeDown)
+        if (!fadeDown) // figure out if we're fading up
         {
-          if (fadeWait == 35)
+          if (fadeWait == 35) // wait 35 loops between fade steps
           {
-            analogWrite(DPIN_GLED, fadeCount++);
-            if (fadeCount == 255)
+            analogWrite(DPIN_GLED, fadeCount++); // increment LED brightness
+            if (fadeCount == 255) // if fully bright, start fading down
             {
               fadeDown = true;
             }
-            fadeWait = 0;
+            fadeWait = 0; // reset fade wait counter
           }
           else
           {
-            fadeWait++;
+            fadeWait++; // increment fade wait counter
           }
         }
-        else if (fadeDown)
+        else if (fadeDown) // if we're fading down
         {
-          if (fadeWait == 25)
+          if (fadeWait == 25) // wait 25 loops between fade steps (looks better on the way down for some reason)
           {
-            analogWrite(DPIN_GLED, fadeCount--);
-            if (fadeCount == 0)
+            analogWrite(DPIN_GLED, fadeCount--); // decrement LED brightness
+            if (fadeCount == 0) // if off, send digital low for true off, then set gledOff to wait for a time before next fade
             {
               fadeDown = false; 
               gledOff = true;
               digitalWrite(DPIN_GLED, LOW);
             }
-            fadeWait = 0;
+            fadeWait = 0; // reset fade wait counter
           }
           else
           {
-            fadeWait++;
+            fadeWait++; // increment fade wait counter
           }
         }
       } 
-      else if (gledOff && fadeOffWait < 2000)
+      else if (gledOff && fadeOffWait < 2000) // this will be true if we're in fade off wait mode
       {
-        fadeOffWait++;
+        fadeOffWait++; // increment fade off wait counter
       }
-      else if (gledOff && fadeOffWait == 2000)
+      else if (gledOff && fadeOffWait == 2000) // waited long enough, get to the next fade
       {
         fadeOffWait = 0;
         gledOff = false;
@@ -216,7 +219,7 @@ void loop()
     {
       digitalWrite(DPIN_GLED, HIGH);
     }
-    else // Hi-Z - shutdown
+    else // Hi-Z - shutdown - not charging
     {
       digitalWrite(DPIN_GLED, LOW);    
     }
@@ -264,11 +267,11 @@ void loop()
   case MODE_PRE_SBL:
     if(mprog)digitalWrite(DPIN_GLED, !sblskip);
 
-    digitalWrite(DPIN_DRV_EN, (time%300)<75);
+    digitalWrite(DPIN_DRV_EN, (time%300)<75); // 300ms loop, 75 ms on
     break;
   case MODE_DAZZLE:
   case MODE_PRE_DAZZLE:
-    if(mprog)digitalWrite(DPIN_GLED, !dazskip);
+    if(mprog)digitalWrite(DPIN_GLED, !dazskip);  // Dazzle code from digitalmisery on GitHub
 
     if (time - lastDazzleTime > dazzle_period)
     {
@@ -278,13 +281,14 @@ void loop()
       dazzle_period = random(25,100);
     } 
     break;
+
   case MODE_FBL:
   case MODE_PRE_FBL:
     if(mprog)digitalWrite(DPIN_GLED, !fblskip);
 
-    digitalWrite(DPIN_DRV_EN, (time%75)<8);
+    digitalWrite(DPIN_DRV_EN, (time%75)<8); // 75 ms loop, 8 ms on
     break;
-  case MODE_PROGRAM:
+  case MODE_PROGRAM: // LMH/HML program mode, most of this code governs the fading of the green LED to demonstrate HML and LMH modes
     if (!gledOff)
     {
       if (hml)
@@ -361,19 +365,19 @@ void loop()
 
   // Check for mode changes
   byte newMode = mode;
-  if (skiptomode != 255) {
+  if (skiptomode != 255) { // check for mode skip and act if necessary
     newMode = skiptomode; 
     skiptomode = 255;
   }
   byte newBtnDown = digitalRead(DPIN_RLED_SW);
-  switch (mode)
+  switch (mode) // button actions are determined here. make sure to use a pre mode if you are acting on a button hold.
   {
   case MODE_OFF:
-    if (hml && btnDown && !newBtnDown && (time-btnTime)>20)
+    if (hml && btnDown && !newBtnDown && (time-btnTime)>20) // button tap
       newMode = MODE_HIGH;
     if (!hml && btnDown && !newBtnDown && (time-btnTime)>20)
       newMode = MODE_LOW;
-    if (btnDown && newBtnDown && (time-btnTime)>500)
+    if (btnDown && newBtnDown && (time-btnTime)>500) // button hold
       newMode = MODE_PRE_DAZZLE;
     break;
   case MODE_PRE_LOW:
@@ -396,7 +400,7 @@ void loop()
       newMode = MODE_PRE_MED;
     break;
   case MODE_PRE_MED:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_MED;
     break;
@@ -415,7 +419,7 @@ void loop()
       newMode = MODE_PRE_HIGH;
     break;
   case MODE_PRE_HIGH:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_HIGH;
     break;
@@ -434,14 +438,14 @@ void loop()
       newMode = MODE_PRE_DAZZLE;
     break;
   case MODE_PRE_DAZZLE:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_DAZZLE;
     if (!mprog && btnDown && newBtnDown && (time-btnTime)>4000)
       newMode = MODE_PRE_PROGRAM;
     break;
   case MODE_PRE_PROGRAM:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_PROGRAM;
     break;
@@ -457,16 +461,16 @@ void loop()
       if (hml) newMode = MODE_PRE_HIGH; 
       else newMode = MODE_PRE_LOW;
       digitalWrite(DPIN_GLED, LOW);
-      mprog = true;
+      mprog = true; // enter mode skip program mode
     }
     break;
   case MODE_OFFHOLD:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_OFF;
     break;
   case MODE_PRE_SBL:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_SBL;
     break;
@@ -477,7 +481,7 @@ void loop()
       newMode = MODE_OFFHOLD;
     if (mprog && btnDown && !newBtnDown && (time-btnTime)>20)
       sblskip = !sblskip;
-    if (mprog && btnDown && newBtnDown && (time-btnTime)>500)
+    if (mprog && btnDown && newBtnDown && (time-btnTime)>500) // last programming step, so check if hml/skip variables match EEPROM and write if necessary, reset all fade counter variables (just in case we're plugged in), then turn the light off.
     {
       if (hml != EEPROM.read(0)) EEPROM.write(0, hml);
       if (highskip != EEPROM.read(1)) EEPROM.write(1, highskip);
@@ -497,7 +501,7 @@ void loop()
     break;
 
   case MODE_PRE_FBL:
-    // This mode exists just to ignore this button release.
+    // This mode exists just to catch this button release.
     if (btnDown && !newBtnDown)
       newMode = MODE_FBL;
     break;
@@ -523,7 +527,7 @@ void loop()
     break;
   }
 
-  // Do the mode transitions
+  // Do the mode transitions (one time execution on mode change)
   if (newMode != mode)
   {
     switch (newMode)
@@ -538,7 +542,7 @@ void loop()
       break;
     case MODE_LOW:
     case MODE_PRE_LOW:
-      if(!mprog && lowskip && hml) {
+      if(!mprog && lowskip && hml) { // skip mode if skip variable is true
         Serial.println("L>O"); 
         skiptomode = MODE_OFF;
       }
@@ -669,10 +673,6 @@ void loop()
     delay(50);
   }
 }
-
-
-
-
 
 
 
